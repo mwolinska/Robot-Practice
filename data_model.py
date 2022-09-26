6aa2d8366fbc14fdcbc65f730dc9d600f13c664c
Marta <mswolinska@gmail.com>
2022-09-26 14:23:44 +0100

    Add Enum to define possible ways to create a robot

--- data_model.py
+++ data_model.py
@@ -7,6 +7,11 @@ class Quadrants (str, Enum):
     BOTTOM_LEFT = "bottom left"
     BOTTOM_RIGHT = "bottom right"
 
+class RobotDesign(str, Enum):
+    USER_INPUT = "user input"
+    RANDOM = "random"
+    FROM_FILE = "from file"
+
 class Compass(str, Enum):
     NORTH = "North"
     SOUTH = "South"
