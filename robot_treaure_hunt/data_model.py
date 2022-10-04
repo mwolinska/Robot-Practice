92def8dd2ef3223e2b11ad2c4e6b626ad3dc635a
Marta <mswolinska@gmail.com>
2022-10-04 11:12:41 +0100

    Remove RobotDesign enum

--- robot_treaure_hunt/data_model.py
+++ robot_treaure_hunt/data_model.py
@@ -7,11 +7,6 @@ class Quadrants (str, Enum):
     BOTTOM_LEFT = "bottom left"
     BOTTOM_RIGHT = "bottom right"
 
-class RobotDesign(str, Enum):
-    USER_INPUT = "user input"
-    RANDOM = "random"
-    FROM_FILE = "from file"
-
 class Compass(str, Enum):
     NORTH = "North"
     EAST = "East"
