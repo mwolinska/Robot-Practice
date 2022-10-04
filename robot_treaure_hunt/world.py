cae7026fac253932762199dd04aa24fd95856918
Marta <mswolinska@gmail.com>
2022-10-04 14:46:02 +0100

    Update World docstrings

--- robot_treaure_hunt/world.py
+++ robot_treaure_hunt/world.py
@@ -12,7 +12,8 @@ class World:
         """Initliase World object.
 
         Args:
-            size: tuple defining grid size of the world board in the order (row, column).
+            size: tuple defining grid size of the world board
+                in the order (row, column).
         """
         self.board = np.zeros((size[1], size[0]))
         self.treasure_position = self._set_random_treasure_position()
@@ -26,22 +27,28 @@ class World:
         Returns:
             Quadrant within which the position is found.
         """
-        if position_tuple[0] > self.board.shape[1] or position_tuple[1] > self.board.shape[0]:
+        if position_tuple[0] > self.board.shape[1] \
+                or position_tuple[1] > self.board.shape[0]:
             raise NameError("Desired position is outside of the board size")
 
         row_boundary = (self.board.shape[1] // 2)
         column_boundary = (self.board.shape[0] // 2)
 
-        if position_tuple[0] < row_boundary and position_tuple[1] < column_boundary:
+        if position_tuple[0] < row_boundary \
+                and position_tuple[1] < column_boundary:
             return Quadrants.TOP_LEFT
-        elif position_tuple[0] <= row_boundary and position_tuple[1] > column_boundary:
+        elif position_tuple[0] <= row_boundary \
+                and position_tuple[1] > column_boundary:
             return Quadrants.TOP_RIGHT
-        elif position_tuple[0] > row_boundary and position_tuple[1] <= column_boundary:
+        elif position_tuple[0] > row_boundary \
+                and position_tuple[1] <= column_boundary:
             return Quadrants.BOTTOM_LEFT
-        elif position_tuple[0] >= row_boundary and position_tuple[1] > column_boundary:
+        elif position_tuple[0] >= row_boundary \
+                and position_tuple[1] > column_boundary:
             return Quadrants.BOTTOM_RIGHT
 
     def _set_random_treasure_position(self):
+        """Select a location along the wall limits to place the treasure."""
         treasure_row = randint(0, 9)
         if treasure_row == 0 or treasure_row == 9:
             treasure_column = randint(0, 9)
